f25375bbdcfdd34abf2f1600fa74ecf38c3b1a54
Alexey Karapetov <karapetov@gmail.com>
2018-11-16 23:35:12 -0800

    Add IMap

--- lib/immutables.dart
+++ lib/immutables.dart
@@ -1,8 +1,2 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library immutables;
-
 export 'src/list.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/map.dart';
